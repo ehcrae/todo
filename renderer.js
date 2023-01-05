bc96980ea882949f02812dff777c6c1ef278f45c
Archie Collison <archie.collison@gmail.com>
2023-01-05 12:22:37 +1100

    functionality: ability to add items to a list

--- renderer.js
+++ renderer.js
@@ -7,21 +7,48 @@
  */
 var itemCount = 0;
 
-function spawnItem() {
+function itemList(text) {
   // create the new item to be added
   const newItem = document.createElement("li");
-  const node = document.createTextNode("Item...");
+  const node = document.createTextNode(text);
   newItem.appendChild(node);
 
   //add the new item to the list
-  const element = document.getElementById("list");
+  const element = document.getElementById("itemList");
   element.appendChild(newItem);
 
   //confirm
   itemCount++;
-  console.log('spawned item', itemCount);
+  console.log('spawned item no.', itemCount, "with value", text);
 }
 
-document.getElementById("spawnItem").onclick = () => {
-  spawnItem();
+function createItem() {
+  // popup field
+  const form = document.getElementById("popupForm");
+  
+  const input = document.createElement("input");
+  input.type = "text";
+  input.id = "itemText";
+  input.name = "itemData";
+  form.appendChild(input);
+
+  const submit = document.createElement("input");
+  submit.type = "submit";
+  submit.id = "itemSubmit";
+  submit.text = "Submit Item";
+  form.appendChild(submit);
+
+  // this onclick creates the new, seemingly empty (of text) item, but instantly removes the entire contents of the <ul> in index.html?
+  document.getElementById('popupForm').addEventListener('submit', (evt) => {
+    // prevent default refresh functionality of forms
+    evt.preventDefault();
+    // return the input to the itemList function
+    const input = evt.target[0];
+    itemList(input.value);
+
+    // reset the input to clean the field
+    input.value = "";
+  })
 }
+// submit button clicked.
+document.getElementById("itemButton").onclick = () => createItem();
